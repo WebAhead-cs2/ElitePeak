ddbbc5ee8a3811fcfcd44f68a2bc247a040eacc2
joumanakh <joumana.1444@gmail.com>
2022-12-03 12:03:00 +0200

    initial project

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const templates = require("./templates");
+const server = express();
+
+server.listen(3000, () =>
+  console.log("Server listening on http://localhost:3000")
+);
+server.get("/", (req, res) => {
+    const html = templates.home();
+    res.send(html);
+  });
---
+++ templates.js
@@ -0,0 +1,34 @@
+function layout(content) {
+    return /*html*/ `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Elite Peak</title>
+          <link rel="stylesheet" href="/style.css">
+        </head>
+        <body>
+          <header>
+            <nav>
+              <a href="/">Home</a>
+              <a href="/new-post">Write new post</a>
+              <a href="/posts">about us</a>
+            </nav>
+          </header>
+          ${content}
+        </body>
+      </html>
+    `;
+  }
+  
+  function home() {
+   
+    
+      return layout(/*html */ `
+        <h1>Elite Peak</h1>
+        <a href="/log-in">Log in</a>
+      `);
+    
+  }
+  module.exports = {home};
