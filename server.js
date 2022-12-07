f818cc3f33487cf0b208f5be1a2e38d5a28f8822
LeenMass <leen.msarwy12@gmail.com>
2022-12-07 12:30:05 +0200

    adding the database

--- server.js
+++ server.js
@@ -1,19 +1,60 @@
 const express = require("express");
 const templates = require("./templates");
 const server = express();
+const db = require("./database/connection");
+const bcrypt = require("bcrypt");
+
 server.use(express.static("assest"));
+server.use(express.urlencoded());
+
 server.listen(3000, () =>
   console.log("Server listening on http://localhost:3000")
 );
 server.get("/", (req, res) => {
-    const html = templates.home();
-    res.send(html);
-  });
+  const html = templates.home();
+  res.send(html);
+});
 server.get("/LogIn", (req, res) => {
-    const html = templates.logIn();
-    res.send(html);
+  const html = templates.logIn();
+  res.send(html);
 });
 server.get("/SignUp", (req, res) => {
   const html = templates.SignUp();
   res.send(html);
 });
+
+server.post("/SignUp", (req, res) => {
+  const {email, password, birthdate, first_name, last_name, country, phone, gender} = req.body;
+  const saltRounds=10;
+  bcrypt
+  .hash(password, saltRounds)
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hash = bcrypt.hashSync(password, salt);
+    db.query(`INSERT INTO users (email, password, birthdate, first_name, last_name, country, phone, gender) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`, [email, hash, birthdate, first_name, last_name, country, phone, gender])
+      .then(() => {
+        res.redirect('/');
+      })
+      .catch(error => {
+        res.status(500).send(`<h1>Something went wrong saving your data</h1>`);
+      });
+});
+server.post("/LogIn",(req,res)=>{
+  const email =  db.query("SELECT email FROM users");
+  const user=req.body.email;
+   console.log(user);
+  console.log(email);
+  const saltRounds=10;
+  bcrypt.hash(password, saltRounds)
+  
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hash = bcrypt.hashSync(password, salt);
+ 
+});
+
+  
+
+server.get("/payment",(req,res)=>{
+ const html=templates.payment();
+  res.send(html);
+});
+
--- templates.js
+++ templates.js
@@ -1,3 +1,5 @@
+const { application } = require("express");
+
 function layout(content) {
     return /*html*/ `
       <!DOCTYPE html>
@@ -12,8 +14,8 @@ function layout(content) {
           <header>
             <nav>
               <a href="/">Home</a>
-              <a href="/new-post">Write new post</a>
-              <a href="/posts">about us</a>
+              <a href="/Reservation">Reservation</a>
+              <a href="/payment">Payment</a>
             </nav>
           </header>
           ${content}
@@ -21,19 +23,18 @@ function layout(content) {
       </html>
     `;
   }
-  
-  function home() {
-      return layout(/*html */ `
-        <h1>Elite Peak</h1>
+
+  function  home() {
+      return layout(/*html*/ `
+         <h1>Elite Peak</h1>
         <a href="/SignUp">Sign Up</a>
         <a href="/LogIn">Log In</a>
-      `);
-    
+     `)
   }
   function logIn() {
     return layout( `
       <h1>Log in to your account</h1>
-      <form action="/log-in">
+      <form  method='post'>
         <label> Email:</label>
         <input id="mail" type="email" name="email" required></br></br>
         <label>Password:<label>
@@ -45,23 +46,105 @@ function layout(content) {
   function SignUp(){
         
     return layout(`
-       <form>
-          <label>Name:<label>
-          <input type='text' id='text' required/><br><br>
+       <form  method='post'>
+          <label>First_Name:<label>
+          <input type='text' id='fname' name='first_name' required/><br><br>
+          <label>Last_Name:<label>
+          <input type='text' id='lname' name='last_name' required/><br><br>
           <label>Phone Number:<label>
-          <input type='tel' id='phone'/><br><br>
+          <input type='tel' id='phone' name='phone'/><br><br>
+          <label>Email:</label>
+          <input id="umail" type="email" name="email" required></br></br>
           <label>Password:</label>
-          <input type='password' id='pass' required/><br><br>
-          <label>Gender:  </label>
+          <input type='password' id='pass' name='password' required/><br><br>
+          <label>Gender:</label>
           <label>Male</label>
-          <input type='radio' id='male' value='male' name='gender'/>
+          <input type='radio' id='male' value='Male' name='gender'/>
           <label>Female</label>
-          <input type='radio' id='female' value='female' name='gender'/></br></br>
+          <input type='radio' id='female' value='Female' name='gender'/></br></br>
+          <label>Country:<label>
+          <input type='text' id='country' name='country'/><br><br>
           <label>Birthdate:</label>
-          <input type='date' id='date' /></br></br>
+          <input type='date' id='date' name='birthdate' /></br></br>
           <input type='submit' id='submit' value='submit' />
-          
        </form>
   `);
   }
-  module.exports = {home,SignUp,logIn};
+  function payment(){
+    return layout(`<section class="p-4 p-md-5" 
+  <div class="row d-flex justify-content-center">
+    <div class="col-md-10 col-lg-8 col-xl-5">
+      <div class="card rounded-3">
+        <div class="card-body p-4">
+          <div class="text-center mb-4">
+            <h3>Settings</h3>
+            <h6>Payment</h6>
+          </div>
+          <form id='form'action="">
+            <p class="fw-bold mb-4 pb-2">Saved cards:</p>
+
+            <div class="d-flex flex-row align-items-center mb-4 pb-1">
+              <img class="img-fluid" src="https://img.icons8.com/color/48/000000/mastercard-logo.png" />
+              <div class="flex-fill mx-3">
+                <div class="form-outline">
+                  <input type="text" id="formControlLgXc" class="form-control form-control-lg"
+                    value="**** **** **** 3193" />
+                  <label class="form-label" for="formControlLgXc">Card Number</label>
+                </div>
+              </div>
+              <a href="#!">Remove card</a>
+            </div>
+
+            <div class="d-flex flex-row align-items-center mb-4 pb-1">
+              <img class="img-fluid" src="https://img.icons8.com/color/48/000000/visa.png" />
+              <div class="flex-fill mx-3">
+                <div class="form-outline">
+                  <input type="text" id="formControlLgXs" class="form-control form-control-lg"
+                    value="**** **** **** 4296" />
+                  <label class="form-label" for="formControlLgXs">Card Number</label>
+                </div>
+              </div>
+              <a href="#!">Remove card</a>
+            </div>
+
+            <p class="fw-bold mb-4">Add new card:</p>
+
+            <div class="form-outline mb-4">
+              <input type="text" id="formControlLgXsd" class="form-control form-control-lg"
+                value="Anna Doe" />
+              <label class="form-label" for="formControlLgXsd">Cardholder's Name</label>
+            </div>
+
+            <div class="row mb-4">
+              <div class="col-7">
+                <div class="form-outline">
+                  <input type="text" id="formControlLgXM" class="form-control form-control-lg"
+                    value="1234 5678 1234 5678" />
+                  <label class="form-label" for="formControlLgXM">Card Number</label>
+                </div>
+              </div>
+              <div class="col-3">
+                <div class="form-outline">
+                  <input type="password" id="formControlLgExpk" class="form-control form-control-lg"
+                    placeholder="MM/YYYY" />
+                  <label class="form-label" for="formControlLgExpk">Expire</label>
+                </div>
+              </div>
+              <div class="col-2">
+                <div class="form-outline">
+                  <input type="password" id="formControlLgcvv" class="form-control form-control-lg"
+                    placeholder="Cvv" />
+                  <label class="form-label" for="formControlLgcvv">Cvv</label>
+                </div>
+              </div>
+            </div>
+
+            <button class="btn btn-success btn-lg btn-block">Add card</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>`)
+  }
+  module.exports = {home,SignUp,logIn,payment};
